21620600036e6aa997b77329afaf0785ffd6e031
ura2rist <ura2rist@gmail.com>
2024-06-03 12:34:43 +0300

    README for backend

---
+++ backend/php/README.md
@@ -0,0 +1,12 @@
+# Frontend building
+## build
+- Download the repository or backend folder
+- Open in the IDE
+- go to the folder `php`
+- run command `composer install`
+- go back to the folder
+- run command `docker compose build`
+- run command `docker compose up -d`
+- run command `docker compose exec php bash`
+- run command `php bin/console make:migration`
+- run command `php bin/console do:mi:mi`
--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bbb4bb1e3574d7d98d4da09effa647688616f998',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bbb4bb1e3574d7d98d4da09effa647688616f998',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
